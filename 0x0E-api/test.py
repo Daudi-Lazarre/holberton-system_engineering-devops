afe5d1fad2eca34bfa4c267828b247e28078b901
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-17 10:51:39 -0500

    Update test.py

--- 0x0E-api/test.py
+++ 0x0E-api/test.py
@@ -10,3 +10,4 @@ with open('names.csv', 'w') as csvfile:
     # writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
     # writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
     # writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+    
